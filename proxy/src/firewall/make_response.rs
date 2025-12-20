2af2c055f214b88d1c6aedcde2acfd359d063d78
glendc <glen@plabayo.tech>
2025-12-20 14:40:07 +0100

    improve content type detection + dynamic package version (malware lists)

--- proxy/src/firewall/make_response.rs
+++ proxy/src/firewall/make_response.rs
@@ -1,9 +1,11 @@
 use rama::http::{
     Request, Response, StatusCode,
-    headers::{self, Accept, HeaderMapExt},
+    headers::{self, Accept, HeaderMapExt, specifier},
     mime,
     service::web::response::{Headers, IntoResponse},
 };
+
+use smallvec::SmallVec;
 
 pub(super) fn generate_blocked_response_for_req(req: Request) -> Response {
     let maybe_detected_ct = req
@@ -80,8 +82,14 @@ enum ContentType {
 
 impl ContentType {
     fn detect_from_accept_header(Accept(qvs): Accept) -> Option<Self> {
-        qvs.iter().find_map(|qv| {
-            let r#type = qv.value.subtype();
+        let mut sorted_qvs: SmallVec<[(mime::Mime, specifier::Quality); 8]> = qvs
+            .into_iter()
+            .map(|qvs| (qvs.value, qvs.quality))
+            .collect();
+        sorted_qvs.sort_by_cached_key(|a| u16::MAX - a.1.as_u16());
+
+        sorted_qvs.iter().find_map(|(value, _)| {
+            let r#type = value.subtype();
             if r#type == mime::JSON {
                 Some(Self::Json)
             } else if r#type == mime::HTML {
@@ -94,5 +102,58 @@ impl ContentType {
                 None
             }
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rama::http::{HeaderValue, headers::HeaderDecode};
+
+    use super::*;
+
+    #[test]
+    fn test_detect_from_accept_header() {
+        for (header_value, expected_result) in [
+            (
+                "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                Some(ContentType::Html),
+            ),
+            ("text/html", Some(ContentType::Html)),
+            (
+                "application/xml,text/html,application/xhtml+xml,*/*;q=0.8",
+                Some(ContentType::Xml),
+            ),
+            ("application/xml", Some(ContentType::Xml)),
+            (
+                "application/xml,text/html,application/xhtml+xml,*/*;q=0.8",
+                Some(ContentType::Xml),
+            ),
+            ("application/xml", Some(ContentType::Xml)),
+            ("text/html;q=0.8,application/xml", Some(ContentType::Xml)),
+            (
+                "text/html;q=0.8,application/json;q=0.9,application/xml,plain/text",
+                Some(ContentType::Xml),
+            ),
+            (
+                "text/html;q=0.8,application/json;q=0.9,plain/text,application/xml",
+                Some(ContentType::Txt),
+            ),
+            (
+                "text/html;q=0.8,application/json;q=0.9,plain/text;q=0.2,application/xml",
+                Some(ContentType::Xml),
+            ),
+            ("plain/text", Some(ContentType::Txt)),
+            ("plain/text; charset=utf8", Some(ContentType::Txt)),
+            ("plain/text; charset=utf8; q=0.5", Some(ContentType::Txt)),
+        ] {
+            let accept =
+                Accept::decode(&mut [&HeaderValue::from_static(header_value)].into_iter()).unwrap();
+            let maybe_ct = ContentType::detect_from_accept_header(accept.clone());
+            assert_eq!(
+                maybe_ct, expected_result,
+                "header value: {header_value}; parsed qvs: {:?}",
+                accept.0
+            );
+        }
     }
 }
