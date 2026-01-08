4d441f3ea2d56fa406f177a627e03d46538da400
Tudor TIMCU <tudor@aikido.dev>
2026-01-08 15:53:02 +0200

    ServiceRunner is back

--- internal/platform/platform_windows.go
+++ internal/platform/platform_windows.go
@@ -96,6 +96,11 @@ func UninstallProxyCA(ctx context.Context) error {
 	return cmd.Run()
 }
 
+type ServiceRunner interface {
+	Start(ctx context.Context) error
+	Stop(ctx context.Context) error
+}
+
 type windowsService struct {
 	runner ServiceRunner
 }
