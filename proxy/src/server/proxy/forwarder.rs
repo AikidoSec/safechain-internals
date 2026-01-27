873cb3b8d37145b2e18a2262a7ff6a7a8aa9f5a3
glendc <glen@plabayo.tech>
2026-01-27 21:38:17 +0100

    support proxy chaining in safechain-proxy

---
+++ proxy/src/server/proxy/forwarder.rs
@@ -0,0 +1,110 @@
+use std::{fmt, sync::Arc};
+
+use rama::{
+    Layer, Service,
+    combinators::Either,
+    error::{BoxError, ErrorContext as _, ErrorExt as _, OpaqueError},
+    extensions::ExtensionsMut,
+    http::client::{ProxyConnector, ProxyConnectorLayer, proxy::layer::HttpProxyConnectorLayer},
+    net::{
+        address::ProxyAddress,
+        client::{ConnectorService, EstablishedClientConnection},
+        proxy::{ProxyRequest, ProxyTarget, StreamForwardService},
+    },
+    proxy::socks5::Socks5ProxyConnectorLayer,
+    rt::Executor,
+    stream::Stream,
+    tcp::client::{Request, service::TcpConnector},
+};
+
+enum ForwarderKind {
+    Direct(TcpConnector),
+    Proxied {
+        connector: ProxyConnector<Arc<TcpConnector>>,
+        proxy_addr: ProxyAddress,
+    },
+}
+
+/// Custom TcpForwarder which can either connect direct,
+/// or via a hardecoded proxy.
+pub(super) struct TcpForwarder {
+    kind: ForwarderKind,
+}
+
+impl fmt::Debug for TcpForwarder {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("TcpForwarder").finish()
+    }
+}
+
+impl TcpForwarder {
+    pub(super) fn new(exec: Executor, proxy: Option<ProxyAddress>) -> Self {
+        let kind = match proxy {
+            Some(proxy_addr) => {
+                let connector = ProxyConnectorLayer::optional(
+                    Socks5ProxyConnectorLayer::required(),
+                    HttpProxyConnectorLayer::required(),
+                )
+                .into_layer(TcpConnector::new(exec));
+                ForwarderKind::Proxied {
+                    connector,
+                    proxy_addr,
+                }
+            }
+            None => ForwarderKind::Direct(TcpConnector::new(exec)),
+        };
+        Self { kind }
+    }
+}
+
+impl<T> Service<T> for TcpForwarder
+where
+    T: Stream + Unpin + ExtensionsMut,
+{
+    type Output = ();
+    type Error = BoxError;
+
+    async fn serve(&self, source: T) -> Result<Self::Output, Self::Error> {
+        let ProxyTarget(host_with_port) = source
+            .extensions()
+            .get()
+            .context("missing forward authority")?;
+
+        let extensions = source.extensions().clone();
+        let mut tcp_req = Request::new_with_extensions(host_with_port.clone(), extensions);
+
+        let target = match &self.kind {
+            ForwarderKind::Direct(connector) => {
+                let EstablishedClientConnection { conn: target, .. } =
+                    connector.connect(tcp_req).await.map_err(|err| {
+                        OpaqueError::from_boxed(err).with_context(|| {
+                            format!("establish direct tcp connection to {host_with_port}")
+                        })
+                    })?;
+                Either::A(target)
+            }
+            ForwarderKind::Proxied {
+                connector,
+                proxy_addr,
+            } => {
+                tcp_req.extensions_mut().insert(proxy_addr.clone());
+                let EstablishedClientConnection { conn: target, .. } =
+                    connector.connect(tcp_req).await.map_err(|err| {
+                        OpaqueError::from_boxed(err).with_context(|| {
+                        format!(
+                            "establish proxied ({proxy_addr}) tcp connection to {host_with_port}"
+                        )
+                    })
+                    })?;
+                Either::B(target)
+            }
+        };
+
+        let proxy_req = ProxyRequest { source, target };
+
+        StreamForwardService::default()
+            .serve(proxy_req)
+            .await
+            .map_err(Into::into)
+    }
+}
