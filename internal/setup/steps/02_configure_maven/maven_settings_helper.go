90f7b4b36e1808e33695fd695d7e9edbe78d7e2c
Reinier Criel <reinier.criel@gmail.com>
2026-02-11 17:24:55 -0800

    Some optimizations

--- internal/setup/steps/02_configure_maven/maven_settings_helper.go
+++ internal/setup/steps/02_configure_maven/maven_settings_helper.go
@@ -7,6 +7,8 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -18,6 +20,9 @@ const (
 	xmlProxiesStart    = "<proxies>"
 	xmlProxiesEnd      = "</proxies>"
 
+	dirPerm  = 0o755
+	filePerm = 0o644
+
 	defaultSettingsTemplate = `<?xml version="1.0" encoding="UTF-8"?>
 <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
           xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
@@ -25,32 +30,48 @@ const (
 </settings>`
 )
 
+// Match an opening <proxies ...> tag.
+var proxiesOpenTagRE = regexp.MustCompile(`(?i)<\s*(?!/)\s*proxies\b[^>]*>`)
+
+func ensureDirForFile(path string) error {
+	return os.MkdirAll(filepath.Dir(path), dirPerm)
+}
+
+func readOrDefaultSettings(settingsPath string) (string, error) {
+	data, err := os.ReadFile(settingsPath)
+	if err == nil {
+		return string(data), nil
+	}
+	if os.IsNotExist(err) {
+		return defaultSettingsTemplate, nil
+	}
+	return "", fmt.Errorf("failed to read settings.xml: %w", err)
+}
+
 func installMavenProxySetting(homeDir, host, port string) error {
 	settingsPath := filepath.Join(homeDir, ".m2", "settings.xml")
 
-	if err := os.MkdirAll(filepath.Dir(settingsPath), 0755); err != nil {
-		return fmt.Errorf("failed to create settings directory: %v", err)
+	if err := ensureDirForFile(settingsPath); err != nil {
+		return fmt.Errorf("failed to create settings directory: %w", err)
 	}
 
-	content := defaultSettingsTemplate
-	if data, err := os.ReadFile(settingsPath); err == nil {
-		content = string(data)
+	content, err := readOrDefaultSettings(settingsPath)
+	if err != nil {
+		return err
 	}
 
-	if stripped, _, err := stripProxyFromSettings(content); err == nil {
-		content = stripped
+	stripped, _, err := stripProxyFromSettings(content)
+	if err != nil {
+		return err
 	}
+	content = stripped
 
 	result, err := applyProxyToSettings(content, host, port)
 	if err != nil {
 		return err
 	}
 
-	if !strings.Contains(result, markerStart) {
-		return fmt.Errorf("failed to apply proxy settings: marker not found in result")
-	}
-
-	return os.WriteFile(settingsPath, []byte(result), 0644)
+	return os.WriteFile(settingsPath, []byte(result), filePerm)
 }
 
 func uninstallMavenProxySetting(homeDir string) error {
@@ -61,29 +82,30 @@ func uninstallMavenProxySetting(homeDir string) error {
 		if os.IsNotExist(err) {
 			return nil
 		}
-		return fmt.Errorf("failed to read settings.xml: %v", err)
+		return fmt.Errorf("failed to read settings.xml: %w", err)
 	}
 
 	newContent, removed, err := stripProxyFromSettings(string(data))
-	if !removed || err != nil {
+	if err != nil {
 		return err
 	}
+	if !removed {
+		return nil
+	}
 
-	return os.WriteFile(settingsPath, []byte(newContent), 0644)
+	return os.WriteFile(settingsPath, []byte(newContent), filePerm)
 }
 
 func applyProxyToSettings(content, host, port string) (string, error) {
+	if err := validateProxyInputs(host, port); err != nil {
+		return "", err
+	}
+
 	proxyBlock := buildProxyBlock(host, port)
 
-	var result string
-	if strings.Contains(content, xmlProxiesStart) {
-		result = strings.Replace(content, xmlProxiesStart, xmlProxiesStart+"\n"+proxyBlock, 1)
-	} else {
-		if !strings.Contains(content, "</settings>") {
-			return "", fmt.Errorf("invalid settings.xml: missing </settings> closing tag")
-		}
-		proxiesBlock := fmt.Sprintf("%s\n%s%s\n", xmlProxiesStart, proxyBlock, xmlProxiesEnd)
-		result = strings.Replace(content, "</settings>", proxiesBlock+"</settings>", 1)
+	result, err := insertProxyBlock(content, proxyBlock)
+	if err != nil {
+		return "", err
 	}
 
 	if err := validateXMLWellFormedness(result); err != nil {
@@ -93,10 +115,42 @@ func applyProxyToSettings(content, host, port string) (string, error) {
 	return result, nil
 }
 
+func insertProxyBlock(content, proxyBlock string) (string, error) {
+	if loc := proxiesOpenTagRE.FindStringIndex(content); loc != nil {
+		openTag := content[loc[0]:loc[1]]
+		if isSelfClosingTag(openTag) {
+			replacement := xmlProxiesStart + "\n" + proxyBlock + xmlProxiesEnd
+			return content[:loc[0]] + replacement + content[loc[1]:], nil
+		}
+
+		// Normal <proxies ...> tag: insert immediately after it.
+		insertAt := loc[1]
+		return content[:insertAt] + "\n" + proxyBlock + content[insertAt:], nil
+	}
+
+	// 3) No proxies section at all: insert one before </settings>.
+	closeIdx := strings.LastIndex(content, "</settings>")
+	if closeIdx == -1 {
+		return "", fmt.Errorf("invalid settings.xml: missing </settings> closing tag")
+	}
+
+	proxiesSection := xmlProxiesStart + "\n" + proxyBlock + xmlProxiesEnd + "\n"
+	return content[:closeIdx] + proxiesSection + content[closeIdx:], nil
+}
+
+func isSelfClosingTag(tag string) bool {
+	// Works for "<proxies/>", "<proxies />", "<proxies foo='bar'/>", etc.
+	tag = strings.TrimSpace(tag)
+	return strings.HasSuffix(tag, "/>")
+}
+
 func stripProxyFromSettings(content string) (string, bool, error) {
 	result, removed, err := removeMarkedBlock(content, markerStart, markerEnd)
-	if !removed || err != nil {
-		return content, false, err
+	if err != nil {
+		return "", false, err
+	}
+	if !removed {
+		return content, false, nil
 	}
 
 	if err := validateXMLWellFormedness(result); err != nil {
@@ -107,6 +161,9 @@ func stripProxyFromSettings(content string) (string, bool, error) {
 }
 
 func buildProxyBlock(host, port string) string {
+	host = xmlEscape(host)
+	port = xmlEscape(port)
+
 	return fmt.Sprintf(`%s
   <proxy>
     <id>%s</id>
@@ -126,6 +183,25 @@ func buildProxyBlock(host, port string) string {
 `, markerStart, aikidoProxyHTTPID, host, port, aikidoProxyHTTPSID, host, port, markerEnd)
 }
 
+func validateProxyInputs(host, port string) error {
+	if strings.TrimSpace(host) == "" {
+		return fmt.Errorf("invalid proxy host")
+	}
+
+	portNum, err := strconv.Atoi(strings.TrimSpace(port))
+	if err != nil {
+		return fmt.Errorf("invalid proxy port")
+	}
+
+	return nil
+}
+
+func xmlEscape(s string) string {
+	var b strings.Builder
+	_ = xml.EscapeText(&b, []byte(s))
+	return b.String()
+}
+
 func removeMarkedBlock(content, startMarker, endMarker string) (string, bool, error) {
 	before, rest, found := strings.Cut(content, startMarker)
 	if !found {
@@ -137,8 +213,7 @@ func removeMarkedBlock(content, startMarker, endMarker string) (string, bool, er
 		return "", false, fmt.Errorf("found start marker but not end marker - corrupt configuration")
 	}
 
-	after = strings.TrimPrefix(after, "\r\n")
-	after = strings.TrimPrefix(after, "\n")
+	after = strings.TrimLeft(after, "\r\n")
 	return before + after, true, nil
 }
 
@@ -151,7 +226,7 @@ func validateXMLWellFormedness(content string) error {
 			return nil
 		}
 		if err != nil {
-			return fmt.Errorf("XML parsing error: %v", err)
+			return fmt.Errorf("XML parsing error: %w", err)
 		}
 	}
 }
