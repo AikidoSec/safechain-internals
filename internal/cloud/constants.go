1a1dcd29cd1189a05b0a48d25be329ab7249f1d3
bitterpanda <bitterpanda@proton.me>
2026-03-03 02:49:09 +0100

    Apply suggestion from @bitterpanda63

--- internal/cloud/constants.go
+++ internal/cloud/constants.go
@@ -1,7 +1,7 @@
 package cloud
 
 const (
-	BaseURL           = "https://app.test.aikido.dev"
+	BaseURL           = "https://app.aikido.dev"
 	HeartbeatEndpoint = "api/endpoint_protection/callbacks/reportDeviceHeartbeat"
 	SBOMEndpoint      = "api/endpoint_protection/callbacks/reportInstalledPackages"
 )
