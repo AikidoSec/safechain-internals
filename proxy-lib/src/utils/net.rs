6ca84c38b741cc2b68c5c414cc6488d4edb63b21
glendc <glen@plabayo.tech>
2026-03-20 17:59:44 +0100

    fix ext flow meta

--- proxy-lib/src/utils/net.rs
+++ proxy-lib/src/utils/net.rs
@@ -7,8 +7,10 @@ pub fn get_app_source_bundle_id_from_ext(_: &impl ExtensionsRef) -> Option<&str>
 
 #[cfg(feature = "apple-networkextension")]
 pub fn get_app_source_bundle_id_from_ext(input: &impl ExtensionsRef) -> Option<&str> {
+    use std::sync::Arc;
+
     input
         .extensions()
-        .get::<rama::net::apple::networkextension::tproxy::TransparentProxyFlowMeta>()
+        .get::<Arc<rama::net::apple::networkextension::tproxy::TransparentProxyFlowMeta>>()
         .and_then(|meta| meta.source_app_bundle_identifier.as_deref())
 }
