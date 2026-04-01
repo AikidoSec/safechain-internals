b376699b20e93f6c190d64f85b2d81004b717535
bitterpanda <bitterpanda@proton.me>
2026-04-01 11:08:56 -0700

    Chrome check /releaases/chrome.json

--- proxy-lib/src/http/firewall/mod.rs
+++ proxy-lib/src/http/firewall/mod.rs
@@ -179,6 +179,7 @@ impl Firewall {
                     layered_client.clone(),
                     data.clone(),
                     policy_evaluator.clone(),
+                    remote_endpoint_config.clone(),
                 )
                 .await
                 .context("create block rule: chrome")?
