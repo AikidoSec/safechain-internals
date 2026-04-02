cc16f2398012d18773122aa3dc559daed919aeb0
Popovici Marian <marian@aikido.dev>
2026-04-02 15:36:51 +0300

    Rename "TLS Failures" to "Logs"

--- ui/frontend/src/pages/TlsEventDetail.tsx
+++ ui/frontend/src/pages/TlsEventDetail.tsx
@@ -75,7 +75,7 @@ export function TlsEventDetail() {
           <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M19 12H5M12 19l-7-7 7-7" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
           </svg>
-          All TLS Failures
+          Logs
         </button>
 
         <div className="tls-detail-header">
