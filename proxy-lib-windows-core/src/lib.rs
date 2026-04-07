b3e748623472a5fdd0b5462f7c22f5daa7c3b461
Glen De Cauwsemaecker <glen@plabayo.tech>
2026-04-07 11:16:43 +0200

    L4 proxy groundwork for Linux and Windows (#271)
    * init prep for l4 proxy (linux/windows)
    
    * add proxy target retrieval code for windows/linux/other
    
    other is just so we can still compile at least on MacOS,
    making it possible to develop most of L4 win/lin proxy on MacOS as well,
    just the windows/linux specific parts you'll need
    a windows/linux for
    
    * make L4 linux/windows proxy single stack
    
    given a dual-stack system can easily become single stack
    with a click of a button, it's best we just run 2 separate proxy instances,
    for dual stack systems... this way the termination of 1 stack
    does not terminate the entire proxy...
    
    Meaning on Linux/Windows you
    
    - run the proxy for ipv4
    - also run the proxy for ipv6 if that system supports ipv6
    
    * mark tcp server ready to listen (l4 win/linux)
    
    * write (tcp) server addr for l4 proxy (linux/windows)
    
    useful if you use auto-port binding,
    not something you would care about if you use a static port instead
    
    * cleanup unused deps

---
+++ proxy-lib-windows-core/src/lib.rs
@@ -0,0 +1,12 @@
+//! Common library used by both the Windows Driver
+//! (in function of Windows L4 Proxy), as well as the
+//! L4 proxy when ran on Windows platforms.
+
+#![cfg_attr(
+    not(test),
+    warn(clippy::print_stdout, clippy::dbg_macro),
+    deny(clippy::unwrap_used, clippy::expect_used)
+)]
+#![no_std]
+
+pub mod redirect_ctx;
