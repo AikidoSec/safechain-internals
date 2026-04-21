8a3b16c3bb4433fbed8fe2d933b8577ff423b6f9
Glen De Cauwsemaecker <glen@plabayo.tech>
2026-04-21 14:16:34 +0200

    migrate remaining windows code

--- proxy-lib/src/utils/net.rs
+++ proxy-lib/src/utils/net.rs
@@ -1,4 +1,5 @@
-use rama::extensions::ExtensionsRef;
+use rama::extensions::{Extension, ExtensionsRef};
+use std::sync::Arc;
 
 #[cfg(not(all(feature = "apple-networkextension", target_os = "macos")))]
 pub fn get_app_source_bundle_id_from_ext(_: &impl ExtensionsRef) -> Option<&str> {
@@ -21,12 +22,17 @@ pub fn get_source_process_path_from_ext(_: &impl ExtensionsRef) -> Option<String
     None
 }
 
+#[derive(Debug, Clone, Extension)]
+pub struct ProxyRedirectContextExt(
+    pub Arc<safechain_proxy_lib_nostd::windows::redirect_ctx::ProxyRedirectContext>,
+);
+
 #[cfg(all(target_os = "windows", feature = "windows-driver"))]
 pub fn get_source_process_path_from_ext(input: &impl ExtensionsRef) -> Option<String> {
     input
         .extensions()
-        .get_ref::<safechain_proxy_lib_nostd::windows::redirect_ctx::ProxyRedirectContext>()
-        .and_then(|ctx| ctx.source_process_path())
+        .get_ref()
+        .and_then(|ProxyRedirectContextExt(ctx)| ctx.source_process_path())
         .map(|s| s.to_owned())
 }
 
