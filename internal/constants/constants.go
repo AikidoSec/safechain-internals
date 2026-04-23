87b19a57d1d8a6a98d015887aae99edc613f6b0b
Tudor TIMCU <tudor@aikido.dev>
2026-04-23 22:27:45 +0300

    Update constants for daemon and logging intervals
    - Standardized formatting of constant declarations in constants.go for improved readability.
    - No functional changes were made to the values of the constants.

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -3,12 +3,12 @@ package constants
 import "time"
 
 const (
-	DaemonHeartbeatInterval = 15 * time.Second // Interval at which the daemon will check the if proxy and scanners are running
-	LogRotationSizeInBytes  = 10 * 1024 * 1024 // 10 MB
-	LogReapingAgeInHours    = 24               // 24 hours
-	DaemonStatusLogInterval = 1 * time.Hour
-	HeartbeatReportInterval = 3 * time.Minute
-	SBOMReportInterval      = 24 * time.Hour
+	DaemonHeartbeatInterval   = 15 * time.Second // Interval at which the daemon will check the if proxy and scanners are running
+	LogRotationSizeInBytes    = 10 * 1024 * 1024 // 10 MB
+	LogReapingAgeInHours      = 24               // 24 hours
+	DaemonStatusLogInterval   = 1 * time.Hour
+	HeartbeatReportInterval   = 3 * time.Minute
+	SBOMReportInterval        = 24 * time.Hour
 	SetupWizardReshowInterval = 24 * time.Hour
-	ProxyStartMaxRetries    = 100
+	ProxyStartMaxRetries      = 100
 )
